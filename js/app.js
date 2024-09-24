93515200631b022ebe90f42bc8b7c8be05becc61
Gustavo Lelis <gustavolelis_bh@hotmail.com>
2024-09-23 22:32:55 -0300

    botões alternando entre alugado e disponivel

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    //let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        botao.classList.remove('dashboard__item__button--return');
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
+    } else {
+        botao.classList.add('dashboard__item__button--return');
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';  
+    }
+
+    //alert(nomeJogo.textContent);
 
 }
